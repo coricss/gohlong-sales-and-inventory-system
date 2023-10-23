9fd6433007e43ecfebaaaf9e5051a94eeb508442
rics <rico.guinanao@fujitsu.com>
2023-10-23 16:40:47 +0800

    feat: added widgets for dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -18,6 +18,34 @@
                     </div>
                 </div>
             </section>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-3 mb-3" v-for="widget in widgets" :key="widget.title">
+                            <Card>
+                                <template #content>
+                                    <div class="d-flex justify-content-between align-items-center">
+                                        <div>
+                                            <h5 class="text-muted">{{ widget.title }}</h5>
+                                            <h4 class="text-dark">{{ widget.count }}</h4>
+                                        </div>
+                                        
+                                        <i :class="widget.icon"></i>
+                                    </div>
+                                </template>
+                                <template #footer>
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        <router-link :to="widget.link" class="text-dark view-details">
+                                            <span>View Details</span>
+                                            <i class="fas fa-angle-right mt-1 ml-2"></i>
+                                        </router-link>
+                                    </div>
+                                </template>
+                            </Card>
+                        </div>
+                    </div>
+                </div>
+            </section>
         <FooterComponent/>
     </div>
 </template>
@@ -26,4 +54,65 @@
     import HeaderComponent from "@/components/layouts/HeaderComponent.vue";
     import SidebarComponent from "@/components/layouts/SidebarComponent.vue";
     import FooterComponent from "@/components/layouts/FooterComponent.vue";
+
+    import {ref, onMounted} from "vue";
+
+    const widgets = ref([
+        {
+            id: 1,
+            title: "Products",
+            icon: "fas fa-box-open fa-3x text-orange widget-icon",
+            count: 1234,
+            link: "/inventory/products"
+        },
+        {
+            id: 2,
+            title: "Categories",
+            icon: "fas fa-shapes fa-3x text-orange widget-icon",
+            count: 1234,
+            link: "/inventory/categories"
+        },
+        {
+            id: 3,
+            title: "Brands",
+            icon: "fas fa-tags fa-3x text-orange widget-icon",
+            count: 1234,
+            link: "/inventory/brands"
+        },
+        {
+            id: 4,
+            title: "Sales",
+            icon: "fas fa-chart-line fa-3x text-orange widget-icon",
+            count: 1234,
+            link: "/inventory/sales"
+        },
+    ]);
+
+    /* update count of categories */
+    const updateCount = () => {
+        widgets.value[0].count = 1000;
+
+    }
+
+    onMounted(() => {
+        updateCount();
+    });
+    
 </script>
+
+<style scoped>
+
+    .p-card .widget-icon {
+        transition: all 0.3s ease-in-out;
+    }
+    .p-card:hover .widget-icon {
+        transform: scale(1.1);
+    }
+
+    .view-details i {
+        transition: all 0.3s ease-in-out;
+    }
+    .view-details:hover i {
+        transform: translateX(5px);
+    }
+</style>
