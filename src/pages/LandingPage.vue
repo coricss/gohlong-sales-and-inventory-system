5f27d99512f0b8d4fb9c18885bd3949cb7a41a47
rics <guinanaorico@gmail.com>
2023-11-08 23:37:55 +0800

    fix: asycn component

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -10,12 +10,29 @@
 </template>
 
 <script setup>
-    import TopHeaderComponent from "@/components/home/TopHeaderComponent.vue";
-    import NavbarComponent from "@/components/home/NavbarComponent.vue";
-    import HomeComponent from "@/components/home/HomeComponent.vue";
-    import ServicesComponent from "@/components/home/ServicesComponent.vue";
-    import BrandsComponent from "@/components/home/BrandsComponent.vue";
-    import GalleryComponent from "@//components/home/GalleryComponent.vue";
-    import ContactComponent from "@//components/home/ContactComponent.vue";
-    import FooterComponent from "@//components/home/FooterComponent.vue";
+    import { defineAsyncComponent } from "vue";
+    const TopHeaderComponent = defineAsyncComponent(() =>
+        import("@/components/home/TopHeaderComponent.vue")
+    );
+    const NavbarComponent = defineAsyncComponent(() =>
+        import("@/components/home/NavbarComponent.vue")
+    );
+    const HomeComponent = defineAsyncComponent(() =>
+        import("@/components/home/HomeComponent.vue")
+    );
+    const ServicesComponent = defineAsyncComponent(() =>
+        import("@/components/home/ServicesComponent.vue")
+    );
+    const BrandsComponent = defineAsyncComponent(() =>
+        import("@/components/home/BrandsComponent.vue")
+    );
+    const GalleryComponent = defineAsyncComponent(() =>
+        import("@//components/home/GalleryComponent.vue")
+    );
+    const ContactComponent = defineAsyncComponent(() =>
+        import("@//components/home/ContactComponent.vue")
+    );
+    const FooterComponent = defineAsyncComponent(() =>
+        import("@/components/home/FooterComponent.vue")
+    );
 </script>
