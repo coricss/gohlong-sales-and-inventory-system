93da8571c3ddb2720f183344817071153dc35ff4
rics <guinanaorico@gmail.com>
2023-12-08 17:58:37 +0800

    fix: router

--- src/router/index.js
+++ src/router/index.js
@@ -69,6 +69,7 @@ const routes = [
 
 const router = createRouter({
     history: createWebHistory(),
+    mode: 'history',
     routes,
     scrollBehavior(to, from, savedPosition) {
       if (savedPosition) {
