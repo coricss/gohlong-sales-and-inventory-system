b25daea5d03e3035757f155fad47f0374a4bacea
rics <guinanaorico@gmail.com>
2023-12-10 21:21:26 +0800

    chore: edited gallery

--- src/components/home/GalleryComponent.vue
+++ src/components/home/GalleryComponent.vue
@@ -11,49 +11,49 @@
                     <div class="image-gallery-wraper">
                     <ul class="image-gallery p-0">
                         <li>
-                        <img src="https://source.unsplash.com/VWcPlbHglYc" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/369022435_1094662891550228_2894152052748992546_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeF3sXFuT_u1QqJsSggPe53zVqr9wi_bGtNWqv3CL9sa0xKoeErHivQKf5dUMjeUZgoeJhTKKebj2frhPPAD8Mis&_nc_ohc=4hS235cWaTIAX93ezA3&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdRoIkdsBE6E6_tbSVrX3LGwr4L2pwKhI1uO4xQpzq-Wow&oe=657BE21D" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/e6FMMambeO4" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/370115283_898748841901803_8569894272033221231_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=102&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeHZgznYQ1U2N3cza3fEBpFaogrt8H8nX4-iCu3wfydfj8puuGZqEWzA-HFlQg9wmmZrWl52MxnqYrBdwWDWAKqR&_nc_ohc=KM1MJxCyKJcAX_JSCOM&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdRsEDSruyqYVMkS0vC2G2-l0L4f0kWbU5tJdVCceXlq7Q&oe=657BDCCA" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/klCiPmzUw0Y" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/371460592_1388888852042668_3454420910162776711_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeHe_qtZwbvyEf83n1txl2WILRoTUgQ1wIwtGhNSBDXAjEiOUsY97AODx5E9ID5Q3b3vOHEt4QDb8LOt54r4Gq20&_nc_ohc=lCA_U6x4aVYAX9-MYwr&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdR2FPn_BEguBISEjFecLCdL3OZTBXWa8llDqFTdkr54TQ&oe=657C09ED" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/IdNOTjPeHrE" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/377222236_1582660332561754_7392387149112623691_n.jpg?stp=dst-jpg_p1080x2048&_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeEgEa1J1NJZpWmTfbw4hTG5JzaKjdNrCU8nNoqN02sJT5zj--siB_aKqxkKM_1vZZrmbwSRgLpd_ahesQMypVac&_nc_ohc=OSAnnZwt4PoAX_BpmPM&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdS_lH5CPIP2CLU-c8EXcM657wNVCtfLmZqi3W3gonI5ZA&oe=657BFB0C" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/O0N9MF--hK4" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/370311693_1024920118771087_6781241936043941574_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=105&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGvAj8kWlffX21OdFyBDCMDygja7DKSLMTKCNrsMpIsxOyl0Kgk1t7rH7N6iwicMaOBpa2doXO6YDBZXpkS1ETM&_nc_ohc=oUnSGVqWizEAX8o50Xu&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdTWIKlgoIcv9MBTPM5B7STp4DdX3XMc0YqUADC8JcHp2A&oe=658006C3" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/FV3GConVSss" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/370274039_1773168749820775_7831878446484181588_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=102&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFZ6hfhIso_VSZQCnt8hfjxyWrF7TgoYcLJasXtOChhwlCWIvs97XFLyPBbri9MWJ7hB-c4ekh9XJPVweZF6Eo1&_nc_ohc=YgRNeAuWmnYAX-kqk-F&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdTML-8Kd-FYqBxIaD3dQ6ZrHx1NE4WdZjDB86593kWMWA&oe=65801FEE" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/0ESjL-Nw22Y" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/386459873_1461846428079390_4330626861988030063_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=102&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeEGGziKDLdJOWJKfwL27X6l97LG3a7A1kn3ssbdrsDWSfY4VaJPwIOBytuuf8LpUDyiZdyV94xsT-GpB_8QD8oI&_nc_ohc=PQOHxI25gTgAX8cGOKG&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdR83VE8GHSQRnHuuETMvhWDnawK7HOtRUz6atn89S2p4g&oe=658012F3" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/KTVn62x6fFw" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/370202303_1859103001173535_8911425689278955089_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=107&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGuEWKwdNT_05kA3KK9lA_paWMqGxDR5m5pYyobENHmbnv9hbO3PAbySZJLCz-T4CCjAj2lwoHQrBJY5rh3FTUM&_nc_ohc=zcQXLuUmvW8AX93rZWn&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdTb3CLx6qvtr5P1hRtUG52YDSiCC-AtZWPxfDck5YKROA&oe=65801E9C" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
 
                         <li>
-                        <img src="https://source.unsplash.com/VSeVhmW4_JQ" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/370077961_275487034981592_7281731654822821195_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=108&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeGTxkjxYMpfOMuvdo900t9O0ge85AcyDujSB7zkBzIO6EQP9r6tGMVG6QNMtSAKRnv2dyTnZcvGu62RAQoTQ_eP&_nc_ohc=oojx3XWM4pEAX8XdUnL&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdSiG4LH97vrGe0G7jCEdQ_Knyvdzp3YVqIR7_b4s_Y7UA&oe=658018D1" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/07aFaTf24Kg" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/386871049_3254253958201412_5906761058680428719_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=111&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeEFspcKs11Q72Fw6Sb2BqPDKPfC-j2ui_Yo98L6Pa6L9haPsK8PycPsv-lfsWlslLJ2qP2O2aCXauoxfqGkBYNz&_nc_ohc=4GJwjLn8p6wAX-mavkn&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdRIyp0vHMRatA-xj69HNYwPoQRsPQX1lRA3YwTkfAe9EQ&oe=658030DE" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                         <li>
-                        <img src="https://source.unsplash.com/DqyYTM7pR2o" alt="" />
-                        <div class="overlay"><span>Image title</span></div>
+                        <img src="https://scontent.fmnl16-1.fna.fbcdn.net/v/t1.15752-9/370070099_828911462250356_2952602500337513165_n.jpg?stp=dst-jpg_s2048x2048&_nc_cat=106&ccb=1-7&_nc_sid=8cd0a2&_nc_eui2=AeFoDruXuZTYLqN2yjoc-YfLBx6FmO1xzEAHHoWY7XHMQJ1RvwehVEH7LE2vgFtchSwgwQmMYIKCTikl6q4Pyk7o&_nc_ohc=b29uIfIdg1UAX_ov49z&_nc_ht=scontent.fmnl16-1.fna&oh=03_AdRECrK2mt1q78l8wEBnYbjisXROM21eEuCJQh93HmNW1Q&oe=65800E7F" alt="" />
+                        <div class="overlay"><span></span></div>
                         </li>
                     </ul>
                     </div>
@@ -81,66 +81,66 @@
     }
 
     .heading-text {
-    margin-bottom: 2rem;
-    font-size: 2rem;
+        margin-bottom: 2rem;
+        font-size: 2rem;
     }
 
     .heading-text span {
-    font-weight: 100;
+        font-weight: 100;
     }
 
     ul {
-    list-style: none;
+        list-style: none;
     }
 
     /* Responsive image gallery rules begin*/
 
     .image-gallery {
-    display: flex;
-    flex-wrap: wrap;
-    gap: 10px;
+        display: flex;
+        flex-wrap: wrap;
+        gap: 10px;
     }
 
     .image-gallery > li {
-    flex: 1 1 auto; /* or flex: auto; */
-    height: 300px;
-    cursor: pointer;
-    position: relative;
+        flex: 1 1 auto; /* or flex: auto; */
+        height: 300px;
+        cursor: pointer;
+        position: relative;
     }
 
     .image-gallery::after {
-    content: "";
-    flex-grow: 999;
+        content: "";
+        flex-grow: 999;
     }
 
     .image-gallery li img {
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-    vertical-align: middle;
-    border-radius: 5px;
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+        vertical-align: middle;
+        border-radius: 5px;
     }
 
     .overlay {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: rgba(57, 57, 57, 0.502);
-    top: 0;
-    left: 0;
-    transform: scale(0);
-    transition: all 0.2s 0.1s ease-in-out;
-    color: #fff;
-    border-radius: 5px;
-    /* center overlay content */
-    display: flex;
-    align-items: center;
-    justify-content: center;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background: rgba(255, 145, 0, 0.267);
+        top: 0;
+        left: 0;
+        transform: scale(0);
+        transition: all 0.2s 0.1s ease-in-out;
+        color: #fff;
+        border-radius: 5px;
+        /* center overlay content */
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
 
     /* hover */
     .image-gallery li:hover .overlay {
-    transform: scale(1);
+        transform: scale(1);
     }
 
 </style>
