6fd14e48e18bec37cfff9f927dc982c9a96ec060
rics <guinanaorico@gmail.com>
2023-12-17 16:38:51 +0800

    feat: added login attempts

--- src/store/auth.js
+++ src/store/auth.js
@@ -36,47 +36,48 @@ export const useAuthStore = defineStore('auth', {
             await axios.post('login', {
                 email: data.email,
                 password: data.password
-            }).then((response) => {
-                
-                if (response.status === 200) {
-                    this.isLoggedIn = true;
-                    sessionStorage.setItem('token', response.data.token);
-                    sessionStorage.setItem('user', JSON.stringify(response.data.user));
-                    /* axios.get('api/user')
-                    .then((response) => {
-                        this.authUser = response.data;
-                        sessionStorage.setItem('user', JSON.stringify(response.data));
-                    }).catch((error) => {
-                        if (error.response.status === 401) {
-                            this.loadToast('Please log in first', 'error');
+            }).then((res) => {
+                if (res.status === 200) {
+                    axios.post('api/check_login_attempts', {
+                        email: data.email
+                    }).then((response) => {
+                        if(response.data.status == 200) {
+                            this.isLoggedIn = true;
+                            sessionStorage.setItem('token', res.data.token);
+                            sessionStorage.setItem('user', JSON.stringify(res.data.user));
+                            this.router.push('/dashboard');
+                        } else if (response.data.status == 401) {
+                            this.loadToast(response.data.message, 'error');
                             this.router.push('/login');
                         }
-                    }); */
-                    setTimeout(() => {
-                        this.router.push('/dashboard');
-                    }, 1000);
+                    });
+
                 } else {
                     this.loadToast('Invalid credentials!', 'error');
                 }
             }).catch((error) => {
                 if (error.response.status === 401) {
-                    this.loadToast('Invalid credentials!', 'error');
+                    this.loadToast(error.response.data.message, 'error');
                     this.router.push('/login');
                 } else if (error.response.status === 422) {
-                    this.loadToast('Invalid credentials!', 'error');
+                    axios.post('api/add_login_attempts', {
+                        email: data.email
+                    }).then((response) => {
+                        this.loadToast(response.data.message, 'error');
+                    }).catch((error) => {
+                        if (error.response.status === 401) {
+                            this.loadToast(error.response.data.message, 'error');
+                            this.router.push('/login');
+                        } else if (error.response.status === 404) {
+                            this.loadToast(error.response.data.message, 'error');
+                            this.router.push('/login');
+                        }
+                    });
                 } else if (error.response.status === 500) {
                     this.loadToast('Server error!', 'error');
                 }
             });
         },
-        /* async handleLogout() {
-            await axios.post('logout').then((response) => {
-                this.authUser = null;
-                sessionStorage.removeItem('token');
-                sessionStorage.removeItem('user');
-                this.router.push('/');
-            });
-        }, */
         async handleLogout() {
             await axios.post('api/logout').then((response) => {
                 if(response.status === 200) {
@@ -91,7 +92,7 @@ export const useAuthStore = defineStore('auth', {
         },
         async loadToast (message, type) {
             toast(message, {
-                timeout: 2000,
+                timeout: 3000,
                 type: type,
                 position: 'top-right',
                 closeOnClick: true,
